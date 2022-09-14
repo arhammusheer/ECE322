62efbb4ad3088e1d4a577db4a3e5833ab7d13e22
arhammusheer <arhammusheer@gmail.com>
2022-09-14 16:59:04 +0000

    Ex11 done

--- Assignment 1/assignment1/ex11.sh
+++ Assignment 1/assignment1/ex11.sh
@@ -5,8 +5,8 @@
 # Problem: Use quotes and escape meta characters to make the script work.
 
 
-echo Jack had $50 until he lost it betting at the races?
-horse=Farlap
+echo Jack had '$50' until he lost it betting at the races?
 
-echo Jack's favourite horse was _$horse_.
+horse=Farlap
+echo Jack\'s favourite horse was _$horse\_.
 
