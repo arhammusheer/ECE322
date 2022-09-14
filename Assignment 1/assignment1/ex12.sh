224a2520ca756eba7a5eaa35c054d5c26fb1a9da
arhammusheer <arhammusheer@gmail.com>
2022-09-14 17:04:04 +0000

    ex12 done

--- Assignment 1/assignment1/ex12.sh
+++ Assignment 1/assignment1/ex12.sh
@@ -5,8 +5,10 @@
 # Problem: Make the code into a function and call function using cat, dog 
 # and frog as an argument.
 
-echo The $1 chased it tail.
-echo Then the $1 sat on a $2.
+functionName (){
+	echo The $1 chased it tail.
+	echo Then the $1 sat on a $2.
+}
 
 functionName cat mat
 functionName dog kennel
